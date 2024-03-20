b8e2351393f29aeb73450d8fb8fc8f93c51f3a60
Conner <connerj.layne@gmail.com>
2024-03-19 20:41:29 -0400

    Added print statement for option 2 and added false for option 3

--- Conner_Layne_Lab6.py
+++ Conner_Layne_Lab6.py
@@ -13,14 +13,17 @@ def main():
             password = encode(password)
             print('Your password has been encoded and stored!')
         elif option == 2:
-            pass
+            print(f'The encoded password is {password}, and the original password is {decode(password)}')
         elif option == 3:
-            pass
+            False
         else:
             print('Invalid option')
         
 
 def encode(string):
+    pass
+
+def decode(string):
     pass
 
 if __name__ == '__main__':

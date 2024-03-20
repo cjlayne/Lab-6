edc34f2e05873e374025e4ee12fc212c66ecd2c5
Conner <connerj.layne@gmail.com>
2024-03-19 20:58:49 -0400

    added code for the encode function

--- Conner_Layne_Lab6.py
+++ Conner_Layne_Lab6.py
@@ -9,7 +9,7 @@ def main():
         option = int(input('Please enter an option: '))
 
         if option == 1:
-            password = int(input('Please enter your password to encode: '))
+            password = input('Please enter your password to encode: ')
             password = encode(password)
             print('Your password has been encoded and stored!')
         elif option == 2:
@@ -20,8 +20,17 @@ def main():
             print('Invalid option')
         
 
-def encode(string):
-    pass
+def encode(password):
+    encoded_password = []
+    for num in password:
+        num = int(num)
+        num += 3
+        if num >= 10:
+            num-= 10
+        encoded_password.append(str(num))
+    encoded_password = ''.join(encoded_password)
+    return encoded_password
+
 
 def decode(string):
     pass

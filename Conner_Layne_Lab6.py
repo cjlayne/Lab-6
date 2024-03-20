efcb3246ab5ecd5bdf1b13455d00d3db13431cff
Conner <connerj.layne@gmail.com>
2024-03-19 20:38:04 -0400

    Added input to get option choice and password and entered code for option 1

--- Conner_Layne_Lab6.py
+++ Conner_Layne_Lab6.py
@@ -5,6 +5,19 @@ def main():
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
+        print()
+        option = int(input('Please enter an option: '))
+
+        if option == 1:
+            password = int(input('Please enter your password to encode: '))
+            password = encode(password)
+            print('Your password has been encoded and stored!')
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        else:
+            print('Invalid option')
         
 
 def encode(string):
